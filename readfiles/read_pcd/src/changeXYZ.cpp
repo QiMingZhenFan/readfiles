203a8a87f8daae5f5bbde6a274a501181d74327e
mrk <ruikai_ma@foxmail.com>
2021-08-26 20:34:22 +0800

    修改changxyz，以及cmakelists加入eigen3

--- readfiles/read_pcd/src/changeXYZ.cpp
+++ readfiles/read_pcd/src/changeXYZ.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "readPcd");
     ros::NodeHandle nh;
     string foldername;
-    string file_name[2] = {"surfaceMap.pcd","cornerMap.pcd"};
+    string file_name[3] = {"surfaceMap.pcd","cornerMap.pcd","finalCloud.pcd"};
 
     if (nh.getParam("foldername", foldername) == false){
         ROS_INFO_STREAM("please set the target PCD file name!");
@@ -30,36 +30,36 @@ int main(int argc, char** argv)
     string key;
     cin >> key;
 
-    int i = 0;
-    while (i < 2) {
+    int i = 2;
+    while (i < 3) {
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_new(new pcl::PointCloud<pcl::PointXYZ>);
+        // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_new(new pcl::PointCloud<pcl::PointXYZ>);
 
         if(pcl::io::loadPCDFile<pcl::PointXYZ>(foldername + file_name[i], *cloud) == -1){
             ROS_ERROR_STREAM("cannot open the PCD file!");
             return -1;
         }
 
-        for(auto data : cloud->points)
+        for(auto &data : cloud->points)
         {
-            pcl::PointXYZ point;
-            point.x = data.z;
-            point.y = data.x;
-            point.z = data.y;
-            cloud_new->points.push_back(point);
+            // pcl::PointXYZ point;
+            // point.x = data.z;
+            // point.y = data.x;
+            // point.z = data.y;
+            // cloud_new->points.push_back(point);
 
             // 为什么这样不行？
-            // float temp;
-            // temp = data.z;
-            // data.z = data.y;
-            // data.y = data.x;
-            // data.x = temp;
+            float temp;
+            temp = data.z;
+            data.z = data.y;
+            data.y = data.x;
+            data.x = temp;
         }
-        cloud_new->width = cloud_new->points.size();
-        cloud_new->height = 1;
-        pcl::io::savePCDFile(foldername + file_name[i], *cloud_new);
+        // cloud_new->width = cloud_new->points.size();
+        // cloud_new->height = 1;
+        pcl::io::savePCDFile(foldername + file_name[i], *cloud);
 
-        ROS_INFO_STREAM(file_name[i] << "cloud height: " << cloud->height << "\twidth: " << cloud->width);
+        ROS_INFO_STREAM(file_name[i] << " cloud height: " << cloud->height << "\twidth: " << cloud->width);
 
         ++i;
 
